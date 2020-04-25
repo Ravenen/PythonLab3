47d82499e421e937706a5a757f1c853b9bd6221a
Raven <redstoyningbros@gmail.com>
2020-04-25 11:29:37 +0300

    Edited parameter

--- model/tree_toy.py
+++ model/tree_toy.py
@@ -6,7 +6,7 @@ from model.decor_type import DecorType
 
 class TreeToy(AbstractDecor):
 
-    def __init__(self, color: str, price_in_uah: float, decor_type: Set[DecorType], form: str, volume_in_cm_cume: int):
+    def __init__(self, color: str, price_in_uah: float, decor_type: Set[DecorType], form: str, volume_in_cm_cube: int):
         super(TreeToy, self).__init__(color, price_in_uah, decor_type)
         self.form = form
-        self.volume_in_cm_cume = volume_in_cm_cume
+        self.volume_in_cm_cube = volume_in_cm_cube
