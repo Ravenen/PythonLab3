939836736b647f04355f7ac01823107418522cfe
Raven <redstoyningbros@gmail.com>
2020-04-25 14:38:28 +0300

    Removed ordering for decor_type set

--- model/abstract_decor.py
+++ model/abstract_decor.py
@@ -9,10 +9,10 @@ class AbstractDecor(ABC):
     def __init__(self, color: str = None, price_in_uah: float = None, decor_type: Set[DecorType] = None):
         self.color = color
         self.price_in_uah = price_in_uah
-        self.decor_type = sorted(decor_type)
+        self.decor_type = decor_type
 
     def check_criterion(self, criterion: Set[DecorType]) -> bool:
-        return set(self.decor_type).issuperset(criterion)
+        return self.decor_type.issuperset(criterion)
 
     def __repr__(self) -> str:
         return f"color: {self.color}, price: {self.price_in_uah}, decor_type: {self.decor_type}"
